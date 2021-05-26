2e991eb30179ea956e0670fb3ebfdc54dd58497f
Hugo Girard <hugogirard28@hotmail.com>
2021-05-26 14:34:18 -0400

    added bicep and remove terraform

--- terraform/modules/cognitive/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_search_service" "search" {
-    name                    = var.searchName
-    resource_group_name     = var.rgName
-    location                = var.location
-    sku                     = "standard"
-}
