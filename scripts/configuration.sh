041786a9af6801ae4d2959b1a6e6417dae8cd8ad
Hugo Girard <hugogirard28@hotmail.com>
2021-07-22 10:20:26 -0400

    modify github action

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -9,4 +9,7 @@ searchServiceName=$1
 apiKey=$2
 cnxStringStorage=$3
 
+# Delete previous configuration
+curl -H "Content-Type: application/json" -H "api-key: $apiKey" -X DELETE "https://$searchServiceName.search.windows.net/datasources/resumesource?api-version=2020-06-30"
+
 curl -d "@datasource.json" -H "Content-Type: application/json" -H "api-key: $apiKey" -X POST "https://$searchServiceName.search.windows.net/datasources?api-version=2020-06-30"
